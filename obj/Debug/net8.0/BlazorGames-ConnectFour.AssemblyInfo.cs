9717d24f9473bcc68e91262996b4a682bd2cc412
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-21 18:34:51 +0000

    Nov 21, 2025, 9:34 PM

--- obj/Debug/net8.0/BlazorGames-ConnectFour.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorGames-ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorGames-ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d7bbf089b41c242e358a9345cd897d5ec78411")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a00bf53619b995e2cbb1620ba274d4f655225e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorGames-ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorGames-ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
